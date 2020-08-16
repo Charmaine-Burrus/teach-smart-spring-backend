12aa6983ac2a4fca3e7904d997bafe2fbcbb8874
Charmaine Burrus <charmaineburrus@yahoo.com>
2020-08-16 16:21:35 -0500

    Fixed formatting in GoogleClassroomService.

--- src/main/java/com/claim/service/GoogleClassroomService.java
+++ src/main/java/com/claim/service/GoogleClassroomService.java
@@ -21,34 +21,34 @@ public class GoogleClassroomService implements CoursesService {
 	@Override
 	public List<Course> listClasses(String authToken){
 		final String uri = "https://classroom.googleapis.com/v1/courses?access_token="+authToken;
-	    
+		
 		RestTemplate restTemplate = new RestTemplate();
-	    	
-	    HttpHeaders headers = new HttpHeaders();
-	    headers.setAccept(Arrays.asList(MediaType.APPLICATION_JSON));
-	
-	    HttpEntity<String> entity = new HttpEntity<String>(headers);
-	
-	    ResponseEntity<ListCoursesResponse> response = restTemplate.getForEntity(uri, ListCoursesResponse.class);
-	    	
-	    //here Jackson provides a java.util.LinkedHashMap representing a List<Course>
-	    List<Course> classes = response.getBody().getCourses();	
-	    return classes;
+		
+		HttpHeaders headers = new HttpHeaders();
+		headers.setAccept(Arrays.asList(MediaType.APPLICATION_JSON));
+		
+		HttpEntity<String> entity = new HttpEntity<String>(headers);
+		
+		ResponseEntity<ListCoursesResponse> response = restTemplate.getForEntity(uri, ListCoursesResponse.class);
+		
+		//here Jackson provides a java.util.LinkedHashMap representing a List<Course>
+		List<Course> classes = response.getBody().getCourses();
+		return classes;
 	}
 	
 	public List<CourseWork> listAssignments(TSClass tSClass) {
 		final String uri = "https://classroom.googleapis.com/v1/courses/" + tSClass.getClassId() + "/courseWork?access_token=" + tSClass.getAccessToken();
-	    	
-	    RestTemplate restTemplate = new RestTemplate();
-	    
-	    HttpHeaders headers = new HttpHeaders();
-	    headers.setAccept(Arrays.asList(MediaType.APPLICATION_JSON));
-	    HttpEntity<String> entity = new HttpEntity<String>(headers);
-	        
-	    ResponseEntity<ListCourseWorkResponse> response = restTemplate.getForEntity(uri, ListCourseWorkResponse.class);
-	    	
-	    //here Jackson provides a java.util.LinkedHashMap representing a List<CourseWork>
-	    List<CourseWork> assignments = response.getBody().getCourseWork();   	
+		
+		RestTemplate restTemplate = new RestTemplate();
+		
+		HttpHeaders headers = new HttpHeaders();
+		headers.setAccept(Arrays.asList(MediaType.APPLICATION_JSON));
+		HttpEntity<String> entity = new HttpEntity<String>(headers);
+		
+		ResponseEntity<ListCourseWorkResponse> response = restTemplate.getForEntity(uri, ListCourseWorkResponse.class);
+		
+		//here Jackson provides a java.util.LinkedHashMap representing a List<CourseWork>
+		List<CourseWork> assignments = response.getBody().getCourseWork();
 		return assignments;
 
 	}
